e40e3020f7c04f48ff6e865432606eef5ed341ca
winnywinbin <62395514+winnywinbin@users.noreply.github.com>
2023-03-01 14:33:56 +0100

    Deleted second backfile

--- src/diversity.R
+++ src/diversity.R
@@ -4,7 +4,6 @@ library(tidyverse)
 #asvtable <- read.csv("~/Desktop/Bioinformatica/Afstuderen/Naturalis/arise-soil-pilot-analysis/data/ASVtab_raw.csv") %>%
 #  select()
 #location1 <- c()
-asvtable <- read.csv("~/Desktop/Bioinformatica/Afstuderen/Naturalis/arise-soil-pilot-analysis/data/ASVtab_raw.csv")
-data("BCI")
+loc1_data <- read.csv("~/Desktop/Bioinformatica/Afstuderen/Naturalis/arise-soil-pilot-analysis/data/location1.csv")
+loc3_data <- read.csv("~/Desktop/Bioinformatica/Afstuderen/Naturalis/arise-soil-pilot-analysis/data/location3.csv")
 
-loc1 <- asvtable["e4057163188"]
