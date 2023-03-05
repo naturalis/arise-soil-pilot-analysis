e4f5956235593dd2c66c2c0ae3ed23894bb9c28e
winnywinbin <62395514+winnywinbin@users.noreply.github.com>
2023-03-05 12:07:21 +0100

    Alpha diversity by OTU next step to split the OTU table by location

--- src/diversity.R
+++ src/diversity.R
@@ -1,9 +1,16 @@
-library(vegan)
-library(tidyverse)
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("metagenomeSeq")
+library(metagenomeSeq)
 
 #asvtable <- read.csv("~/Desktop/Bioinformatica/Afstuderen/Naturalis/arise-soil-pilot-analysis/data/ASVtab_raw.csv") %>%
 #  select()
 #location1 <- c()
-loc1_data <- read.csv("~/Desktop/Bioinformatica/Afstuderen/Naturalis/arise-soil-pilot-analysis/data/location1.csv")
-loc3_data <- read.csv("~/Desktop/Bioinformatica/Afstuderen/Naturalis/arise-soil-pilot-analysis/data/location3.csv")
+loc1_data <- read.csv("~/Desktop/Bioinformatica/Afstuderen/Naturalis/arise-soil-pilot-analysis/data/location1.csv", header = TRUE)
+loc3_data <- read.csv("~/Desktop/Bioinformatica/Afstuderen/Naturalis/arise-soil-pilot-analysis/data/location3.csv", header = TRUE)
 
+head(loc1_data)[,1:6]
+
+richness <- estimate_richness(loc1_data)
+head(richness)
