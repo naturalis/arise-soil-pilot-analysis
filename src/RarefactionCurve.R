168cb9d84594caf29f534fa3c666ba4c4483699a
winnywinbin <62395514+winnywinbin@users.noreply.github.com>
2023-04-18 10:23:43 +0200

    Deleted extra files, Rarefactioncurve succes PNG and file

--- src/RarefactionCurve.R
+++ src/RarefactionCurve.R
@@ -11,11 +11,33 @@ loc1 <- t(loc1)
 loc1zt <- as.data.frame(t(loc1zt))
 loc1zt <- tibble::rownames_to_column(loc1zt, "enummers")
 
-loc1 <- read_tsv(loc1zt) %>%
-  select(enummers, starts_with("OTU")) %>%
-  pivot_longer(-Group) %>%
-  group_by(Group)
+loc1zt_df <- loc1zt %>%
+  pivot_wider(names_from = "N", values_from = "value", values_fill = 0) %>%
+  as.data.frame()
+### -> Error in `pivot_wider()`: ! Can't subset columns that don't exist.
 
+loc1zt_df <- as.data.frame(loc1zt)
+rownames(loc1zt_df) <- loc1zt_df$enummers
+loc1zt_df <- loc1zt_df[,-1]
+
+### step = enige outup is per stappen van 50
+rarecurve_data <- rarecurve(loc1zt_df, step = 50)
+
+### Pakt elk element van de list rarecurve_data, en bind het samen tot een dataframe
+map_dfr(rarecurve_data, bind_rows) %>% 
+  bind_cols(Group = rownames(loc1zt_df),.) %>%
+  pivot_longer(-Group) %>%
+  drop_na() %>%
+  mutate(n_seq = as.numeric(str_replace(name, "N", ""))) %>%
+  select(-name) %>%
+  ggplot(aes(x=n_seq, y=value, group=Group)) +
+  geom_line()
+
+# drarefy: probability of seeing a specific taxa for sampling effort
+loc1zt_df %>%
+  bind_cols(Group = rownames(loc1zt_df),.) %>%
+  group_by(Group) %>%
+  summarize(n_seq = sum(value))
 
 
 my_rarefy <- function(x, sample){
